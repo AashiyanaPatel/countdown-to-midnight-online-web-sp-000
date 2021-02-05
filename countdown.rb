d9e013b64544fdafa44e6b15e2bbc0570bde026b
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 00:57:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,13 +2,14 @@ def countdown(num)
   while num > 0
   puts "#{num} SECOND(S)!"
   num -= 1
-"HAPPY NEW YEAR!"
-  end
 end
+"HAPPY NEW YEAR!"
+end
+
 
 def countdown_with_sleep(num)
   sleep(1)
   num -= 1
+end
 "HAPPY NEW YEAR!"
-  end
 end
